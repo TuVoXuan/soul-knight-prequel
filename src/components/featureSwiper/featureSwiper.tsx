285240e41b86eb7d58a5977061673a6ee818c342
TuVoXuan <voxuantu1235@gmail.com>
2024-02-28 09:59:04 +0700

    responsive feature game swiper

--- src/components/featureSwiper/featureSwiper.tsx
+++ src/components/featureSwiper/featureSwiper.tsx
@@ -1,12 +1,16 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import clsx from "clsx";
+import useWindowSize from "../../hooks/useWindowSize";
 
 type Props = {};
 
 const FeatureSwiper = (props: Props) => {
 	const [activeIndex, setActiveIndex] = useState<number>(0);
+	const [isDesktopScreen, setIsDesktopScreen] = useState<boolean>(false);
+
 	const featureSwiperRef = useRef<HTMLUListElement>(null);
+	const windowSize = useWindowSize();
 
 	const handleClickNavigationButton = (type: "next" | "prev") => {
 		if (featureSwiperRef.current) {
@@ -62,83 +66,98 @@ const FeatureSwiper = (props: Props) => {
 		}
 	};
 
+	useEffect(() => {
+		if (windowSize) {
+			let tempImgUrls = [];
+			if (windowSize < 992) {
+				setIsDesktopScreen(false);
+			} else {
+				setIsDesktopScreen(true);
+			}
+		}
+	}, [windowSize]);
+
+	useEffect(() => {
+		const autoSlideTimeout = setTimeout(() => {
+			handleClickNavigationButton("next");
+		}, 3000);
+
+		return () => {
+			clearTimeout(autoSlideTimeout);
+		};
+	}, [activeIndex]);
+
 	return (
 		<div className="relative z-20 flex flex-col items-center justify-center w-full h-full">
-			<ul ref={featureSwiperRef} className="feature-swiper relative h-[390px] w-[230px]">
-				<li className="feature-swiper-slide-active">
-					<Image
-						src={require("@/assets/images/img-feature-1-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-next">
-					<Image
-						src={require("@/assets/images/img-feature-2-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-slide-not-active">
-					<Image
-						src={require("@/assets/images/img-feature-3-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-slide-not-active">
-					<Image
-						src={require("@/assets/images/img-feature-4-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-slide-not-active">
-					<Image
-						src={require("@/assets/images/img-feature-5-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-slide-not-active">
-					<Image
-						src={require("@/assets/images/img-feature-6-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-slide-not-active">
-					<Image
-						src={require("@/assets/images/img-feature-7-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
-				<li className="feature-swiper-prev">
-					<Image
-						src={require("@/assets/images/img-feature-8-m.webp")}
-						alt="feature game"
-						// className="h-[375px] w-[212px]"
-						quality={100}
-					/>
-				</li>
+			<ul
+				ref={featureSwiperRef}
+				className="feature-swiper relative h-[390px] w-[230px] md:w-[316px] md:h-[536px] lg:w-[691px] lg:h-[421px]"
+			>
+				{!isDesktopScreen ? (
+					<>
+						<li className="feature-swiper-slide-active">
+							<Image src={require("@/assets/images/img-feature-1-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-next">
+							<Image src={require("@/assets/images/img-feature-2-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-3-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-4-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-5-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-6-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-7-m.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-prev">
+							<Image src={require("@/assets/images/img-feature-8-m.webp")} alt="feature game" quality={100} />
+						</li>
+					</>
+				) : (
+					<>
+						<li className="feature-swiper-slide-active">
+							<Image src={require("@/assets/images/img-feature-1.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-next">
+							<Image src={require("@/assets/images/img-feature-2.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-3.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-4.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-5.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-6.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-slide-not-active">
+							<Image src={require("@/assets/images/img-feature-7.webp")} alt="feature game" quality={100} />
+						</li>
+						<li className="feature-swiper-prev">
+							<Image src={require("@/assets/images/img-feature-8.webp")} alt="feature game" quality={100} />
+						</li>
+					</>
+				)}
 			</ul>
 
 			<div className="feature-swiper-navigation">
 				<li
 					onClick={() => handleClickNavigationButton("prev")}
-					className="bg-size-full z-[200] w-[23px] h-[43px] rotate-180 bg-[url('../assets/images/arrow-feature-right.png')]"
+					className="bg-size-full z-[200] w-[23px] h-[43px] md:w-8 md:h-[60px] rotate-180 bg-[url('../assets/images/arrow-feature-right.png')]"
 				></li>
 				<li
 					onClick={() => handleClickNavigationButton("next")}
-					className="bg-size-full z-[200] w-[23px] h-[43px] bg-[url('../assets/images/arrow-feature-right.png')]"
+					className="bg-size-full z-[200] w-[23px] h-[43px] md:w-8 md:h-[60px] bg-[url('../assets/images/arrow-feature-right.png')]"
 				></li>
 			</div>
 
